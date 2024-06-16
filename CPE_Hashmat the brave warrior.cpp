4396b16c6469bba1a2626871f3209de2cbfbd81d
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2024-06-16 21:01:43 +0800

    Add files via upload

---
+++ CPE_Hashmat the brave warrior.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std ;
+int main() {
+  long long int input1,input2 ;
+  while( cin >> input1 >> input2 ) {
+    input1 = abs( input1 - input2) ;
+    cout << input1 << endl ;
+  }
+  
+  
+  
+}
