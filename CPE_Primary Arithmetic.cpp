d5037034444c45b34050b54214d0cf9bb1cdd669
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2024-06-17 23:09:26 +0800

    Add files via upload

---
+++ CPE_Primary Arithmetic.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char input1[10], input2[10] ;
+  int tail1, tail2, count = 0, num1, num2 ;
+  bool carry = false ;
+  while ( scanf("%s %s", input1, input2) != EOF ) {
+    tail1 = strlen(input1)-1 ;
+    tail2 = strlen(input2)-1 ;
+    
+    if ( ( strlen(input1) == 1 ) && ( strlen(input2)== 1 ) && ((input1[tail1] - '0') == 0)  && (( input2[tail2] - '0' )== 0) )
+      break ;
+    while( tail1 >=0 && tail2 >=0 ) {
+      num1 = input1[tail1] - '0', num2 = input2[tail2] - '0' ;
+      if ( carry ) {
+        num1++ ;
+        carry = false ;
+      }
+      
+      if ( ( num1 + num2 ) > 9 ) {
+        carry = true ;
+        count++ ;
+      }
+      //printf("%d %d\n", num1, num2 ) ;
+      //printf("%c %c\n", input1[tail1], input2[tail2] ) ;
+      tail1--;
+      tail2--;
+      
+      
+    }
+    
+    //printf("%d .\n", count ) ;
+    if ( carry ) {
+      if ( tail1 >= 0 ) {
+        while ( tail1 >= 0 ) {
+          num1 = input1[tail1] - '0' ; 
+          if ((  num1 + 1 ) > 9 ){
+            carry = true ;
+            count++ ;
+            tail1-- ;
+          }
+          else 
+            break ;
+        }
+      }
+      else if ( tail2 >= 0 ) {
+        while ( tail2 >= 0 ) {
+          num2 = input2[tail2] - '0' ; 
+          if ((  num2 + 1 ) > 9 ){
+            carry = true ;
+            count++ ;
+            tail2-- ;
+          }
+          else 
+            break ;
+        }
+      }
+    }
+    
+    if ( count == 0 )
+      printf("No carry operation.\n" ) ;
+    else if ( count > 1 ) 
+      printf("%d carry operations.\n", count ) ;
+    else
+      printf("%d carry operation.\n", count ) ;
+    count = 0 ;
+    carry = false ;
+    
+    
+    
+  }
+}
